f68f3f5bcda82f6b990fdbebcd791da87ec54707
LDHugeman <luisdavid.vp@gmail.com>
2020-12-26 16:09:20 +0100

    clase 19

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ProductComponent } from './components/product/product.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ExponentialPipe } from './pipes/exponential.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent],
+  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
