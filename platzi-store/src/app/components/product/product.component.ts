02ee1748f8ae6d2534dbbd026f92447e18b83a9b
LDHugeman <luisdavid.vp@gmail.com>
2020-12-26 13:26:22 +0100

    clase 15

--- platzi-store/src/app/components/product/product.component.ts
+++ platzi-store/src/app/components/product/product.component.ts
@@ -1,13 +1,44 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { Product } from 'src/app/interfaces/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  /*ngOnChanges(changes: SimpleChanges): void {
+    console.log('2. ngOnChanges');
+    console.log(changes);
+  }*/
+
+  ngOnInit(): void {
+    console.log('3. ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy(): void {
+    console.log('5. ngOnDestroy');
+  }
 
   addCart(): void {
     console.log('añadir al carrito');
