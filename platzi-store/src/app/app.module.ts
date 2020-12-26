0e71ea72e7cb5e292caf78a622dd7e39c9ca6682
LDHugeman <luisdavid.vp@gmail.com>
2020-12-26 19:17:05 +0100

    clase 28

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -7,19 +7,31 @@ import { ProductComponent } from './components/product/product.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ExponentialPipe } from './pipes/exponential.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
-import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective, HomeComponent, ProductsComponent, ContactComponent, DemoComponent, HeaderComponent, FooterComponent, BannerComponent, PageNotFoundComponent, ProductDetailComponent, LayoutComponent],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ExponentialPipe,
+    HighlightDirective,
+    ProductsComponent,
+    ContactComponent,
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    ProductDetailComponent,
+    LayoutComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
