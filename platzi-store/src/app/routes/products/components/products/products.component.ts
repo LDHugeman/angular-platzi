cb11ba5bacd0ae4302fb87d60f290a262e2933b8
LDHugeman <luisdavid.vp@gmail.com>
2020-12-27 15:58:10 +0100

    clase 37

--- platzi-store/src/app/routes/products/components/products/products.component.ts
+++ platzi-store/src/app/routes/products/components/products/products.component.ts
@@ -18,6 +18,13 @@ export class ProductsComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
-    this.products = this.productsService.getAllProducts();
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.productsService.getAllProducts().subscribe((products) => {
+      console.log(products);
+      this.products = products;
+    });
   }
 }
