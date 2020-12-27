365479aa22689541d63a41107208456981cc8a82
LDHugeman <luisdavid.vp@gmail.com>
2020-12-27 21:18:02 +0100

    clase 46

--- platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
+++ platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/services/products.service';
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
@@ -9,11 +10,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormProductComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
   ngOnInit(): void {}
+
+  saveProduct(event: Event): void {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((newProduct) => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
 
   private buildForm(): void {
     this.form = this.formBuilder.group({
